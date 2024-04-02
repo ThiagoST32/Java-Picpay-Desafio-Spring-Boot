681fcdde1d389652978702aedc0000e865d3be6b
ThiagoST32 <thiago.sandre.trevisan@gmail.com>
2024-04-02 05:05:55 -0400

    Criação do metodo de UpdateUser

--- src/main/java/estudos/ProjetoCrud/services/UserService.java
+++ src/main/java/estudos/ProjetoCrud/services/UserService.java
@@ -3,6 +3,7 @@ package estudos.ProjetoCrud.services;
 import estudos.ProjetoCrud.domain.User;
 import estudos.ProjetoCrud.domain.UserType;
 import estudos.ProjetoCrud.dto.UserDTO;
+import estudos.ProjetoCrud.dto.UserUpdateDTO;
 import estudos.ProjetoCrud.repositories.UserRepositories;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -18,10 +20,11 @@ public class UserService {
     private UserRepositories userRepositories;
 
     public void verifyTypeUser(User sender, BigDecimal tokens) throws Exception {
-        if (sender.getUserType() == UserType.Receiver){
-            throw new Exception ("O usuario não pode fazer transações");
-        }if (sender.getTokens().compareTo(tokens) <= 0){
-            throw  new Exception("O usuario não possui tokens suficientes");
+        if (sender.getUserType() == UserType.Receiver) {
+            throw new Exception("O usuario não pode fazer transações");
+        }
+        if (sender.getTokens().compareTo(tokens) <= 0) {
+            throw new Exception("O usuario não possui tokens suficientes");
         }
     }
 
@@ -29,30 +32,43 @@ public class UserService {
         return this.userRepositories.findById(id).orElseThrow(() -> new Exception("Usuario não encontrado!!"));
     }
 
-    public User createUser(UserDTO data){
+    public User createUser(UserDTO data) {
         User newUser = new User(data);
         this.saveUser(newUser);
         System.out.println(data);
         return newUser;
     }
 
-    public void saveUser(User user){
+    public void saveUser(User user) {
         this.userRepositories.save(user);
     }
 
-    public List<User> getAllUsers(){
+    public List<User> getAllUsers() {
         System.out.println(this.userRepositories.findAll());
         return this.userRepositories.findAll();
     }
 
     @Transactional
-    public void deleteUser(Long id){
+    public void deleteUser(Long id) {
         try {
             this.userRepositories.findById(id);
-
         } catch (Exception e) {
             throw new RuntimeException("Não foi possível deletar o usuario!!");
         }
+
+    }
+
+    @Transactional
+    public User updateUser(UserUpdateDTO userUpdateDTO, Long id) throws Exception {
+        User newObjUser = findByIdUser(id);
+        if (id == newObjUser.getId()){
+            newObjUser.setUserType(userUpdateDTO.userType());
+            newObjUser.setEmail(userUpdateDTO.email());
+            newObjUser.setFirstName(userUpdateDTO.firstName());
+            newObjUser.setLastName(userUpdateDTO.lastName());
+            System.out.println(userUpdateDTO);
+            return this.userRepositories.save(newObjUser);
+        }throw new Exception("O id do usuario n existe");
     }
 
 }
